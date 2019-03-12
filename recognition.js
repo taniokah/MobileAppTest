4f9a06c4580bfe1cecaae35e9896105b536dfa00
Hiroki <xxyy_z@pb3.so-net.ne.jp>
2019-03-12 10:26:54 +0900

    Update recognition.js

--- recognition.js
+++ recognition.js
@@ -1,4 +1,6 @@
-var recognition = new webkitSpeechRecognition();
+window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
+var recognition = new SpeechRecognition();
+//var recognition = new webkitSpeechRecognition();
   recognition.onresult = function(event) {
   if (event.results.length > 0) {
     q.value = event.results[0][0].transcript;
